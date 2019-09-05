66b79c1f867da2b06ee4a973546f23cf4de02084
mary5haw <865332676@qq.com>
2019-09-05 13:45:15 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
 
-    </div>
+    <canvas id="mainCanvas"
+            style=" width:'400px';height:'300px'"></canvas>
+
     <!-- <router-view /> -->
   </div>
 </template>
@@ -10,32 +11,34 @@
 
 <script>
 import * as THREE from "three"
+function init () {
+  // renderer
+  var renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('mainCanvas')
+  });
+  renderer.setClearColor(0x000000); // black
 
+  // scene
+  var scene = new THREE.Scene();
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // camera
+  var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
+  camera.position.set(0, 0, 5);
+  scene.add(camera);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  // a cube in the scene
+  var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3),
+    new THREE.MeshBasicMaterial({
+      color: 0xff0000
+    })
+  );
+  scene.add(cube);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-var animate = function () {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  // render
   renderer.render(scene, camera);
-};
+}
+init()
 
-animate();
 </script>
 
 
